1647df7224306cf3c3616d1d5e3bb20b8bd58d1b
Ermias Aweke <ermiasdagnewa@gmail.com>
2026-02-17 18:09:42 +0300

    Update serializers.py

--- feed/serializers.py
+++ feed/serializers.py
@@ -1,11 +1,7 @@
-# feed/serializers.py
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Post, Comment, Like
 
-# --------------------------
-# User serializers
-# --------------------------
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -19,20 +15,16 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'password']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        return User.objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
             password=validated_data['password']
         )
-        return user
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
 
-# --------------------------
-# Post serializers
-# --------------------------
 class PostSerializer(serializers.ModelSerializer):
     author = UserSerializer(read_only=True)
     likes_count = serializers.SerializerMethodField()
@@ -48,15 +40,6 @@ class PostSerializer(serializers.ModelSerializer):
     def get_comments_count(self, obj):
         return obj.comments.count()
 
-    def get_likes_count(self, obj):
-        return obj.likes.count()
-
-    def get_comments_count(self, obj):
-        return obj.comments.count()
-
-# --------------------------
-# Comment serializers
-# --------------------------
 class CommentSerializer(serializers.ModelSerializer):
     author = UserSerializer(read_only=True)
 
@@ -64,9 +47,6 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = ['id', 'post', 'content', 'author', 'created_at']
 
-# --------------------------
-# Like serializers
-# --------------------------
 class LikeSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
 
