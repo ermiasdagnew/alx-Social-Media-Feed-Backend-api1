3280e225b4eb6a9ad89351e8a5ee0d59cd3b665f
Ermias Aweke <ermiasdagnewa@gmail.com>
2026-02-17 18:11:27 +0300

    Update queries.py

--- feed/queries.py
+++ feed/queries.py
@@ -1,7 +1,7 @@
 import graphene
 from graphene_django.types import DjangoObjectType
 from django.contrib.auth.models import User
-from .models import Post, Comment, Interaction
+from .models import Post, Comment
 
 class UserType(DjangoObjectType):
     class Meta:
@@ -15,16 +15,20 @@ class CommentType(DjangoObjectType):
     class Meta:
         model = Comment
 
-class InteractionType(DjangoObjectType):
-    class Meta:
-        model = Interaction
-
 class Query(graphene.ObjectType):
-    all_posts = graphene.List(PostType)
-    post_by_id = graphene.Field(PostType, id=graphene.Int())
+    all_posts = graphene.List(PostType, first=graphene.Int(), skip=graphene.Int())
+    post_by_id = graphene.Field(PostType, id=graphene.Int(required=True))
 
-    def resolve_all_posts(self, info):
-        return Post.objects.all().select_related('author').prefetch_related('comments')
+    def resolve_all_posts(self, info, first=None, skip=None):
+        qs = Post.objects.all().select_related('author').prefetch_related('comments').order_by('-created_at')
+        if skip:
+            qs = qs[skip:]
+        if first:
+            qs = qs[:first]
+        return qs
 
     def resolve_post_by_id(self, info, id):
-        return Post.objects.get(id=id)
+        try:
+            return Post.objects.get(id=id)
+        except Post.DoesNotExist:
+            return None
