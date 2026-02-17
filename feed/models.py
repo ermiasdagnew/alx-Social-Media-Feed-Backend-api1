b139b1d32c615e2b7d97eac275bead74c8584078
Ermias Aweke <ermiasdagnewa@gmail.com>
2026-02-17 18:08:10 +0300

    Update models.py

--- feed/models.py
+++ feed/models.py
@@ -5,8 +5,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)  # add this
-
+    updated_at = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
