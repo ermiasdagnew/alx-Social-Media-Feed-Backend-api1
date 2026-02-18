8efae701d01d15f25350bad2a10c04c4bcbb5589
Ermias Aweke <ermiasdagnewa@gmail.com>
2026-02-18 14:40:53 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -136,10 +136,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # DJANGO REST FRAMEWORK
 # --------------------------------------------------
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    # You can remove the global permission if you want per-view control
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ],
 }
+
 
 # --------------------------------------------------
 # GRAPHQL
